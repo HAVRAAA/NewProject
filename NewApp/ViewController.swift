fca2ab8caf173096d9782418eeaed7a8ad3e8d84
HAVRAAA <havrilyukmaksim@gmail.com>
2023-06-27 19:12:54 +0300

    Created a project

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Максим Гаврилюк on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
